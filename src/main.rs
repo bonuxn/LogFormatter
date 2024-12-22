c0df607b3e6f3031a7f551f9b0c65792fe99c78e
bonusun <delacs1739@gmail.com>
2024-12-23 07:35:31 +0900

    [update] fileチェック処理を記載途中

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::Parser;
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    file: String,
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello {}!", args.file);
+
+    let path = Path::new(&args.file);
+}
+
+fn ifJsonFile(path : &Path) -> bool {
+    let mut result : bool = false;
+
+    // file check
+    if(!path.is_file()) {
+        println!("This is not a file");
+        return result;
+    }
+    // json check
+    let json: Result<Value, serde_json::Error> = from_reader::<BufReader<File>, Value>(reader);
+    let json = match json {
+        Ok(o) => o,
+        Err(e) => {
+            println!("Failed to parse json.");
+            return Err(Box::new(e));
+        }
+    };
+    result = true;
+    return result;
+
 }
