500e404153af478157edf942140e1ba15fc604aa
Ryu Maruta <Ryu1_Maruta@toshibatec.co.jp>
2024-12-27 18:25:00 +0900

    [update] 正規化表現でチェックするところがビルド通っていない

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use clap::Parser;
 use std::path::Path;
 use serde::Deserialize;
 use serde_json;
-use std::fs::{read_to_string, File};
-use std::io::{BufRead, BufReader, Read};
+use std::fs::{create_dir_all,read_to_string, File};
+use std::io::{self,BufWriter, Write,BufRead, BufReader, Read};
+use std::collections::HashMap;
+use std::env;
+use regex::Regex;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,7 +21,9 @@ struct Args {
 #[serde(rename_all = "camelCase")]
 struct SettingsJson {
     file_path: String,
-    target_words: TargetWords
+    target_words: TargetWords,
+    date_format: String,
+    time_format: String
 }
 
 #[derive(Debug, Deserialize)]
@@ -27,7 +32,7 @@ struct TargetWords {
     words: Vec<String>,
     reg_ex: bool
 }
-
+const BOM: &[u8; 3] = &[0xEF, 0xBB, 0xBF];
 
 fn main() {
     let args = Args::parse();
@@ -57,19 +62,58 @@ fn main() {
         println!("This is not a file");
         return;
     }
+
     let lines = read_lines(&path.display().to_string());
-    let mut hitData : Vec<String> = Vec::new();
+    let mut hit_data = HashMap::new();
+    let mut hit_data_no :u64 = 1;
+    let target_words : Vec<String> = setting_json_data.target_words.words;
+    let date_format : String = setting_json_data.date_format;
+    let time_format : String = setting_json_data.time_format;
+
+    println!("dateFormat={},timeFormat={}",date_format,time_format);
+
+    hit_data.insert(hit_data_no,concat!("date","\t", "time","\t","data"));
+
+    // search target word -> input hitData
+    let regex_date = Regex::new(&date_format).expect("Invalid date format regex");
+    let regex_time = Regex::new(&time_format).expect("Invalid date format regex");
+
     for line in lines {
-        for targetWord in setting_json_data.target_words.words.iter() {
-            if line.contains(targetWord) {
-                println!("{}",line);
+        for target_word in target_words.iter() {
+            if line.contains(target_word) {
+                if regex_date.is_match(&line) && regex_time.is_match(&line) {
+                    hit_data_no += 1;
+                    let date_caps = regex_date.captures(&line).unwrap();
+                    let time_caps = regex_time.captures(&line).unwrap();
+                    let date_data = date_caps.get(0).unwrap().as_str();
+                    let time_data = time_caps.get(0).unwrap().as_str();
+                    hit_data.insert(hit_data_no,&format!("{} {} {} {} {}", &date_data, "\t", &time_data, "\t", &line));
+                    println!("{} : {} : {}",date_data,time_data,line);
+                }
             } else {
                 // no hit
             }
         }
     }
-
+    write_csv_file(hit_data).expect("TODO: panic message");
     // read 1line on file and search TargetWords
+}
+
+
+fn extract_date_from_string(input: &str, date_format: &str) -> Option<String> {
+
+    // Create a regex object from the date_format string
+    let regex = Regex::new(date_format).expect("Invalid date format regex");
+
+    // Find the first match in the input string
+    if let Some(captures) = regex.captures(input) {
+        // Return the matching date substring
+        if let Some(matched) = captures.get(0) {
+            return Some(matched.as_str().to_string());
+        }
+    }
+
+    None
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
@@ -78,4 +122,37 @@ fn read_lines(filename: &str) -> Vec<String> {
         .lines()  // split the string into an iterator of string slices
         .map(String::from)  // make each slice into a string
         .collect()  // gather them together into a vector
+}
+
+fn write_csv_file(data: HashMap<u64, &str>) -> Result<(), String> {
+    // 出力先ディレクトリを設定
+    let output_dir = env::current_dir().unwrap();
+    create_directory(&output_dir)?;
+
+    // ファイルパスを生成し、ファイルを作成
+    let file_path = output_dir.join("fruits.csv");
+    let file = File::create(&file_path).map_err(|e| format!("Failed to create file: {}", e))?;
+
+    // ファイル書き込み処理
+    let mut writer = BufWriter::new(file);
+    write_bom(&mut writer)?;
+    write_csv_content(&mut writer, &data)?;
+
+    Ok(())
+}
+fn create_directory(path: &Path) -> Result<(), String> {
+    create_dir_all(path).map_err(|e| format!("Failed to create directory: {}", e))
+}
+
+fn write_bom(writer: &mut BufWriter<File>) -> Result<(), String> {
+    writer
+        .write_all(BOM)
+        .map_err(|e| format!("Failed to write BOM: {}", e))
+}
+
+fn write_csv_content(writer: &mut BufWriter<File>, data: &HashMap<u64, &str>) -> Result<(), String> {
+    for (_, value) in data {
+        writeln!(writer, "{}", value).map_err(|e| format!("Failed to write line: {}", e))?;
+    }
+    Ok(())
 }
